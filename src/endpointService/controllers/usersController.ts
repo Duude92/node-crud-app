eb0c6f2c1b6aa7574b262a827a22eb7987cd2fd5
Duude92 <switch4acc@gmail.com>
2025-05-07 16:16:30 +0300

    Update app users controller to use db provider

--- src/endpointService/controllers/usersController.ts
+++ src/endpointService/controllers/usersController.ts
@@ -1,22 +1,27 @@
 import { isValidUser, IUser } from '../../shared/models/userModel';
 import { ServerResponse, IncomingMessage } from 'node:http';
 import { ApiController } from '../../shared/endpointFunctionPair';
-import { randomUUID, UUID } from 'node:crypto';
+import { randomUUID } from 'node:crypto';
+import * as storageProvider from '../../providers/userStorageProvider';
 
-const users: Array<IUser> = [
-  { id: '1', age: 2, hobbies: ['aboba'], username: 'name' } as IUser,
-  { id: '12', age: 23, hobbies: ['aboba1'], username: 'nam1e' } as IUser
-];
 const uuidRegex = /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
+const contentType = { 'Content-Type': 'application/json' };
 
 const getUsers = async (res: ServerResponse<IncomingMessage>): Promise<void> => {
   // await provider get users
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(users));
+  const result = await storageProvider.getUsers();
+  if (result.ok) {
+    const users = await result.json() as IUser[];
+    res.writeHead(200, contentType);
+    res.end(JSON.stringify(users));
+    return;
+  }
+  res.writeHead(result.status, contentType);
+  res.end();
 };
 const validateUUID = (id: string, res: ServerResponse<IncomingMessage>) => {
   if (!uuidRegex.test(id)) {
-    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.writeHead(400, contentType);
     res.end(`User id "${id}" is not valid UUIDv4 record.`);
     const error = new Error(`Invalid UUID: ${id}`);
     error.name = 'EINUUID';
@@ -24,24 +29,19 @@ const validateUUID = (id: string, res: ServerResponse<IncomingMessage>) => {
   }
 };
 const getUser = async (res: ServerResponse<IncomingMessage>, id: string): Promise<void> => {
-  try {
-    validateUUID(id, res);
-    const user = users.find(x => x.id === id);
-    if (!user) {
-      res.writeHead(404, { 'Content-Type': 'application/json' });
-      res.end(`Invalid User ID: ${id}`);
-      return;
-    }
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+  validateUUID(id, res);
+  const result = await storageProvider.getUser(id);
+  if (result.ok) {
+    const user = await result.json() as IUser;
+    res.writeHead(200, contentType);
     res.end(JSON.stringify(user));
-  } catch {
     return;
   }
 };
 const validateUser = (body: IUser, res: ServerResponse<IncomingMessage>) => {
   const valid = isValidUser(body);
   if (!valid) {
-    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.writeHead(400, contentType);
     res.end(`Object missing required fields.
 Valid keys are:
 username: string;
@@ -52,56 +52,40 @@ hobbies: string[];`);
 };
 const postUser = async (res: ServerResponse<IncomingMessage>, body: IUser): Promise<void> => {
   validateUser(body, res);
-  body.id = randomUUID().toString();
-  users.push(body);
-  res.writeHead(201, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(body));
-};
-
-const findUser = (id: string, res: ServerResponse<IncomingMessage>): IUser => {
-  const user = users.find(x => x.id === id);
-  if (!user) {
-    responseNotFound(res, id);
-    const error = new Error('User not found!');
-    error.name = 'ENOTFOUND';
-    throw error;
+  const result = await storageProvider.postUser(body);
+  if (result.ok) {
+    const user = await result.json() as IUser;
+    res.writeHead(201, contentType);
+    res.end(JSON.stringify(user));
+    return;
   }
-  return user;
 };
 
 const responseNotFound = (res: ServerResponse<IncomingMessage>, id: string) => {
-  res.writeHead(404, { 'Content-Type': 'application/json' });
+  res.writeHead(404, contentType);
   res.end(`User with id ${id} not found.`);
 };
 
 const deleteUser = async (res: ServerResponse<IncomingMessage>, id: string): Promise<void> => {
-  try {
-    validateUUID(id, res);
+  validateUUID(id, res);
 
-    const user = findUser(id, res);
-    const userIdx = users.indexOf(user);
-
-    users.splice(userIdx, 1);
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+  const result = await storageProvider.deleteUser(id);
+  if (result.ok) {
+    const user = await result.json() as IUser;
+    res.writeHead(200, contentType);
     res.end('User deleted successfully.');
-  } catch {
     return;
   }
 };
 
 const putUser = async (res: ServerResponse<IncomingMessage>, id: string, body: IUser): Promise<void> => {
-  try {
-    validateUUID(id, res);
-    validateUser(body, res);
-
-    const user = findUser(id, res);
-    const userIdx = users.indexOf(user);
-
-    body.id = user.id;
-    users[userIdx] = body;
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(body));
-  } catch {
+  validateUUID(id, res);
+  validateUser(body, res);
+  const result = await storageProvider.putUser(id, body);
+  if (result.ok) {
+    const user = await result.json() as IUser;
+    res.writeHead(200, contentType);
+    res.end(JSON.stringify(user));
     return;
   }
 };
