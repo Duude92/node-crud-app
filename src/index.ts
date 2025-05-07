08c5ae93d170d4ca2f202a36b5b2aaa12b2dab30
Duude92 <switch4acc@gmail.com>
2025-05-07 15:10:53 +0300

    Initialize db service

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { startServer } from './endpointService/serverStartup';
 import dotenv from 'dotenv';
 import { controller } from './endpointService/controllers/usersController';
+import { initDbService } from './dbService/init';
 
 dotenv.config();
 
+initDbService(+(process.env.DB_PORT || 5050));
 // Default behaviour is 3000 port
 startServer(+(process.env.APP_PORT || 3000), controller);
