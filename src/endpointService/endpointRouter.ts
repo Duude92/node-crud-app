6d6ccf64dadea85c1d2188837d3f2a653a7b5208
Duude92 <switch4acc@gmail.com>
2025-05-07 10:05:47 +0300

    Wrapped whole router into try-catch for now

--- src/endpointService/endpointRouter.ts
+++ src/endpointService/endpointRouter.ts
@@ -4,22 +4,26 @@ import { requestBody } from './bodyRequestUtil';
 import { IUser } from '../shared/models/userModel';
 
 export const handleRoute = async (request: IncomingMessage, response: ServerResponse<IncomingMessage>, endpoints: EndpointFunctionPair) => {
-  const method = request.method;
-  const reqPath = (request.url as string).split('/');
-  reqPath.shift();
-  const pathApi = reqPath.shift();
-  const controllerPath = reqPath.shift() as string;
-  let parameter: any;
-  let endpoint: string;
-  if (method === 'POST') {
-    parameter = await requestBody(request);
-    endpoint = `${method}${parameter.id ? '/id' : ''}`;
-  } else {
-    parameter = /*method === 'POST' ? request.body : */reqPath.shift() as string;
-    endpoint = `${method}${parameter ? '/id' : ''}`;
+  try {
+    const method = request.method;
+    const reqPath = (request.url as string).split('/');
+    reqPath.shift();
+    const pathApi = reqPath.shift();
+    const controllerPath = reqPath.shift() as string;
+    let parameter: any;
+    let endpoint: string;
+    if (method === 'POST') {
+      parameter = await requestBody(request);
+      endpoint = `${method}${parameter.id ? '/id' : ''}`;
+    } else {
+      parameter = /*method === 'POST' ? request.body : */reqPath.shift() as string;
+      endpoint = `${method}${parameter ? '/id' : ''}`;
+    }
+    // Should contain METHOD{/?{parameter?}}
+    const fn = endpoints[endpoint];
+    fn(response, parameter);
   }
-  // Should contain METHOD{/?{parameter?}}
-
-  const fn = endpoints[endpoint];
-  fn(response, parameter);
+  catch (error) {
+    console.error(error);
+  }
 };
