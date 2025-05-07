9810556d9b7faff1d2f21f1288dded2adcbc5ba4
Duude92 <switch4acc@gmail.com>
2025-05-07 11:51:52 +0300

    Added user validation function

--- src/shared/models/userModel.ts
+++ src/shared/models/userModel.ts
@@ -1,6 +1,13 @@
 export interface IUser {
-  id: string;
+  id?: string;
   username: string;
   age: number;
   hobbies: string[];
 }
+
+const defaultKeys = Object.keys({ username: 'default', age: 0, hobbies: ['default'] });
+export const isValidUser = (user: IUser) => {
+  const keys = Object.keys(user);
+  const missingKey = defaultKeys.find((key) => !keys.includes(key));
+  return !missingKey;
+};
