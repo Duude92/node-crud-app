fcc837ad48f455da865ef27ac538c512146f4134
Duude92 <switch4acc@gmail.com>
2025-05-07 09:49:49 +0300

    Added processor for post methods to retrieve body from request data

--- src/endpointService/endpointRouter.ts
+++ src/endpointService/endpointRouter.ts
@@ -1,15 +1,24 @@
 import { IncomingMessage, ServerResponse } from 'node:http';
 import { EndpointFunctionPair } from './endpointFunctionPair';
+import { requestBody } from './bodyRequestUtil';
+import { IUser } from '../shared/models/userModel';
 
-export const handleRoute = (request: IncomingMessage, response: ServerResponse<IncomingMessage>, endpoints: EndpointFunctionPair) => {
+export const handleRoute = async (request: IncomingMessage, response: ServerResponse<IncomingMessage>, endpoints: EndpointFunctionPair) => {
   const method = request.method;
   const reqPath = (request.url as string).split('/');
   reqPath.shift();
   const pathApi = reqPath.shift();
   const controllerPath = reqPath.shift() as string;
-  const parameter = reqPath.shift() as string;
+  let parameter: any;
+  let endpoint: string;
+  if (method === 'POST') {
+    parameter = await requestBody(request);
+    endpoint = `${method}${parameter.id ? '/id' : ''}`;
+  } else {
+    parameter = /*method === 'POST' ? request.body : */reqPath.shift() as string;
+    endpoint = `${method}${parameter ? '/id' : ''}`;
+  }
   // Should contain METHOD{/?{parameter?}}
-  const endpoint = `${method}${parameter ? '/id' : ''}`;
 
   const fn = endpoints[endpoint];
   fn(response, parameter);
